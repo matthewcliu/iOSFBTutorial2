fdd7eaa8edd0e0158adbc155f0ad5324f5df06d8
Matthew Liu <matthewliu@gmail.com>
2013-06-17 14:44:11 -0700

    Initial Commit

---
+++ iOSFBTutorial2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSFBTutorial2
+//
+//  Created by Matthew Liu on 6/17/13.
+//  Copyright (c) 2013 Matthew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
